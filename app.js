5152554cf75f0863e24d26d632d776c54339f190
Jacopo Pace <my.root.access@gmail.com>
2017-06-20 21:03:14 +0200

    lecture 6

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 (function () {
 'use strict'
 
-angular.module('myFirstApp', [])
+angular.module('nameCalculator', [])
 
-.controller('MyFirstController', function ($scope) {
-    $scope.name = 'Jacopo'
-    $scope.sayHello = function() {
-        return 'Hello!'
+.controller('nameCalculatorController', function ($scope) {
+    $scope.name = ''
+    $scope.totalValue = 0
+
+    //$scope.totalValueFun = function() {
+    //     return $scope.name
+    //}
+
+    $scope.displayNumeric = function() {
+        var totalNameValue = calculateNumericForString($scope.name)
+        $scope.totalValue = totalNameValue
+    }
+
+    function calculateNumericForString(string) {
+        var val = 0
+        for (var i = 0; i < string.length; i++) {
+            val += string.charCodeAt(i)
+        }
+        return val
     }
 })
 
